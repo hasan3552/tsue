aa47dbb5210d61d830ba272b282792db7d146a26
hasan <hasan6453552"gmail.com>
2022-09-10 14:33:26 +0500

    Initial commit

---
+++ src/main/java/com/company/TsueApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsueApplication.class, args);
+    }
+
+}
